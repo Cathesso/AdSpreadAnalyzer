76daf57e2279bc5f339d1c6767a32c46677a9338
Cathesso <h-f.fromme@posteo.de>
2022-01-22 21:41:25 +0100

    Second Push

---
+++ backend/adspreadanalyzer/src/main/java/de/cathesso/adspreadanalyzer/AdspreadanalyzerApplication.java
@@ -0,0 +1,13 @@
+package de.cathesso.adspreadanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdspreadanalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdspreadanalyzerApplication.class, args);
+	}
+
+}
---
+++ backend/adspreadanalyzer/src/test/java/de/cathesso/adspreadanalyzer/AdspreadanalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.cathesso.adspreadanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdspreadanalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
